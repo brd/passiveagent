4d40076417b097d57342c5726fd6f5de2dee7cb7
Brad Davis <brd@FreeBSD.org>
2021-08-14 11:11:28 -0600

    Add a slightly more complicated config with multiple servers

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,3 +8,7 @@ def test_simple_config():
   assert c['nrdp']['parent'] == '127.0.0.1'
   assert c['nrdp']['token'] == 'foobar'
   assert c['passive checks']['%%HOSTNAME%%|ZPOOL'] == 'check_zpool.sh -p ALL'
+
+def test_complicated_config():
+  c = config.read_config('tests/complicated')
+  assert c['nrdp']['parent'] == 'https://server1.example.org/nrdp, https://server2.example.org/nrdp'
