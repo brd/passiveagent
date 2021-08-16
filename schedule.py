64f84b1679d75ddd1cb494df6895ce0929239bd5
Brad Davis <brd@FreeBSD.org>
2021-08-15 18:25:50 -0600

    Add missing scheduler priority

--- schedule.py
+++ schedule.py
@@ -7,7 +7,7 @@ def clear_sched(s):
 def start_sched(config):
   s = sched.scheduler()
   for c in config['passive checks']:
-    s.enter(config['passive checks'][c]['interval'] - 7,
+    s.enter(config['passive checks'][c]['interval'] - 7, 1
       run_test(config['passive checks'][c]['command'])
 
   s.run
