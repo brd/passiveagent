f6aa9c54cac1d6bceb5ca9d82193a553d77cb858
Brad Davis <brd@FreeBSD.org>
2021-08-22 13:18:34 -0600

    Fix check name

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,7 +6,7 @@ def test_simple_config():
   c = config.read_config('tests/simple')
   assert c['nrdp']['parent'] == ['127.0.0.1']
   assert c['nrdp']['token'] == 'foobar'
-  assert c['passive checks']['%%hostname%%|zpool']['command'] == 'check_zpool.sh -p ALL'
+  assert c['passive checks']['%%hostname%%|zpool']['command'] == 'check_zpools -p ALL'
   assert c['passive checks']['%%hostname%%|zpool']['interval'] == 300
 
 def test_complicated_config():
