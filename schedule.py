ceaba301bbbfe6bd7fde8eed277b908ee8ee5a74
Brad Davis <brd@FreeBSD.org>
2021-08-22 12:43:00 -0600

    Fix some syntax errors

--- schedule.py
+++ schedule.py
@@ -7,8 +7,8 @@ def clear_sched(s):
 def start_sched(config):
   s = sched.scheduler()
   for c in config['passive checks']:
-    s.enter(config['passive checks'][c]['interval'] - 7, 1
-      run_test(config['passive checks'][c]['command'])
+    s.enter(config['passive checks'][c]['interval'] - 7, 1,
+      run_test(config['passive checks'][c]['command']))
 
-  s.run
+  s.run()
   return s
