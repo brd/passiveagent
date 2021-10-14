ca25b90a923a7c0b9b9fde51fb8e1aa5ee2963a0
Brad Davis <brd@FreeBSD.org>
2021-10-14 16:51:38 -0600

    Add pidfile creation and handling

--- lite.py
+++ lite.py
@@ -2,6 +2,7 @@
 
 import argparse
 import logging
+import os
 import requests
 import signal
 import sys
@@ -26,7 +27,12 @@ def handle_sighup(signal, frame):
 
 def handle_exit(signum, frame):
   logging.warning('%s recieved, exiting..', signal.strsignal(signum))
+  remove_pid(c['pidfile'])
   sys.exit(0)
+
+def remove_pid(pidfile):
+  if os.path.isfile(pidfile):
+    os.remove(pidfile)
 
 def main():
   parser = argparse.ArgumentParser(description="Passive Agent")
@@ -35,12 +41,26 @@ def main():
     help='override the location of the configdir')
   parser.add_argument('-l', '--logfile', default='/var/log/lite.log',
     help='override the location of the logfile')
+  parser.add_argument('-p', '--pidfile',
+    default='/var/run/passiveagent.pid',
+    help='override the location of the pidfile')
   args = parser.parse_args()
   c['config_dir'] = args.configdir
+
   # Setup logging
   logging.basicConfig(filename=args.logfile, level=logging.INFO,
     format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %I:%M:%S')
   logging.info('Starting up..')
+
+  # Setup pidfile
+  c['pidfile'] = args.pidfile
+  try:
+    with open(c['pidfile'], "w") as p:
+      p.write(os.getpid())
+  except:
+    logging.error('Unable to create pidfile: %s', c['pidfile'])
+    sys.exit(2)
+
   initialize_signal_handlers()
 
   config.read_config(c)
