ba81de317dea195a0a2cf3d1856091e0a6539fa0
Brad Davis <brd@FreeBSD.org>
2021-10-15 09:22:28 -0600

    Daemonize by default, but have an option to run int he foreground

--- lite.py
+++ lite.py
@@ -34,11 +34,45 @@ def remove_pid(pidfile):
   if os.path.isfile(pidfile):
     os.remove(pidfile)
 
+def daemonize():
+  try:
+    pid = os.fork()
+    if pid > 0:
+      sys.exit(0)
+  except OSError as err:
+    sys.stderr.write('Fork #1 failed: {0}\n'.format(err))
+    sys.exit(1)
+
+  # detach from parent environment
+  os.chdir('/')
+  os.setsid()
+  os.umask(0)
+  # second fork
+  try:
+    pid = os.fork()
+    if pid > 0:
+      # exit from second parent
+      sys.exit(0)
+  except OSError as err:
+    sys.stderr.write('Fork #2 failed: {0}\n'.format(err))
+    sys.exit(1)
+  # redirect std file descriptors
+  sys.stdout.flush()
+  sys.stderr.flush()
+  stdin  = open(os.devnull, 'r')
+  stdout = open(os.devnull, 'w')
+  stderr = open(os.devnull, 'w')
+  os.dup2(stdin.fileno(), sys.stdin.fileno())
+  os.dup2(stdout.fileno(), sys.stdout.fileno())
+  os.dup2(stderr.fileno(), sys.stderr.fileno())
+
 def main():
   parser = argparse.ArgumentParser(description="Passive Agent")
   parser.add_argument('-c', '--configdir',
     default='/usr/local/etc/passiveagent',
     help='override the location of the configdir')
+  parser.add_argument('-f', '--foreground', action='store_true',
+    help='Do not detach and become a deamon')
   parser.add_argument('-l', '--logfile', default='/var/log/lite.log',
     help='override the location of the logfile')
   parser.add_argument('-p', '--pidfile',
@@ -46,6 +80,10 @@ def main():
     help='override the location of the pidfile')
   args = parser.parse_args()
   c['config_dir'] = args.configdir
+
+  # Detach from the controlling terminal
+  if not args.foreground:
+    daemonize()
 
   # Setup logging
   logging.basicConfig(filename=args.logfile, level=logging.INFO,
