5c310ad9cc026f36419371a7b15d498da367f865
Brad Davis <brd@FreeBSD.org>
2021-10-20 17:19:38 -0600

    Prep for 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='passiveagent',
-  version='0.1.9',
+  version='0.2.0',
   install_requires=['requests'],
   packages=['libpassiveagent'],
   scripts=['passiveagent.py']
